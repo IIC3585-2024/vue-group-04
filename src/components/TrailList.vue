a21e31b0f4890b1378c296bc8bc8caeadd16d48a
domingo2000 <dedwards@buk.cl>
2024-05-23 18:31:07 -0400

    refactor: extraer lista de vista y coordinar componentes en vista de trails

--- src/components/TrailList.vue
+++ src/components/TrailList.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="trail-list">
-    <SearchBar :handleSearch="handleSearch" placeholder="Search for a trail" />
-    <div class="trail-list__content">
-      <div v-for="trail in trails" :key="trail.id">
-        <RouterLink :to="'/trails/' + trail.id">
-          <TrailCard
-            :trail="trail"
-            :isFavorite="favoritesStore.isFavorite(trail.id)"
-            @handle-favorite="favoritesStore.toggleFavorite"
-          />
-        </RouterLink>
-      </div>
+    <div v-for="trail in trails" :key="trail.id">
+      <RouterLink :to="'/trails/' + trail.id">
+        <TrailCard
+          :trail="trail"
+          :isFavorite="favoritesStore.isFavorite(trail.id)"
+          @handle-favorite="favoritesStore.toggleFavorite"
+        />
+      </RouterLink>
     </div>
   </div>
 </template>
@@ -18,44 +15,31 @@
 <script setup lang="ts">
 import type { Trail } from '../types/Trail'
 import TrailCard from '../components/TrailCard.vue'
-import SearchBar from '../components/SearchBar.vue'
 import { useFavoritesStore } from '../stores/favorites'
-import { onMounted, ref } from 'vue'
-import { getTrails } from '@/services/trailsService'
 
-const trails = ref<Trail[]>([])
+defineProps<{
+  trails: Trail[]
+}>()
 
 const favoritesStore = useFavoritesStore()
-
-const handleSearch = async (value: string) => {
-  trails.value = await getTrails({ title: value })
-}
-
-onMounted(async () => {
-  trails.value = await getTrails()
-})
 </script>
 
 <style scoped lang="scss">
 .trail-list {
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  gap: 2rem;
+  margin: 0 var(--spacing-4);
 
-  &__content {
-    display: grid;
-    gap: 2rem;
+  @media (min-width: 1200px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
 
-    @media (min-width: 1200px) {
-      grid-template-columns: repeat(3, 1fr);
-    }
+  @media (min-width: 992px) and (max-width: 1199px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 
-    @media (min-width: 992px) and (max-width: 1199px) {
-      grid-template-columns: repeat(2, 1fr);
-    }
-
-    @media (max-width: 991px) {
-      grid-template-columns: repeat(1, 1fr);
-    }
+  @media (max-width: 991px) {
+    grid-template-columns: repeat(1, 1fr);
   }
 }
 </style>
--- src/views/TrailsView.vue
+++ src/views/TrailsView.vue
@@ -1,21 +1,40 @@
 <script setup lang="ts">
 import TrailList from '../components/TrailList.vue'
+import { onMounted, ref } from 'vue'
+import { getTrails } from '@/services/trailsService'
+import type { Trail } from '@/types/Trail'
+import SearchBar from '../components/SearchBar.vue'
 
+const trails = ref<Trail[]>([])
+
+const handleSearch = async (value: string) => {
+  trails.value = await getTrails({ title: value })
+}
+
+onMounted(async () => {
+  trails.value = await getTrails()
+})
 </script>
 
 <template>
   <main>
-    <div class="flex">
+    <div class="trails-index">
       <h1>Explore some trails</h1>
-      <div class="container">
-        <TrailList/>
-      </div>
+      <SearchBar :handleSearch="handleSearch" placeholder="Search for a trail"/>
+      <TrailList :trails />
     </div>
   </main>
 </template>
 
 <style scoped lang="scss">
-.trail-list {
-  margin: 2rem;
+.search-bar {
+  width: 70%;
+}
+
+.trails-index {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: var(--spacing-2);
 }
 </style>
