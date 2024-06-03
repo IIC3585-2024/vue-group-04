c3c54e8d0b6780b1e7f5e81094bc338f38224c95
Martin Gago <mgagol@uc.cl>
2024-06-02 22:02:26 -0400

    feat: add map

--- scrapper/add_coordinates.py
+++ scrapper/add_coordinates.py
@@ -1,22 +1,23 @@
 import json
 import random
 
-
+# Leer el archivo JSON
 with open('./db.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
 # Función para generar coordenadas aleatorias dentro de Chile
 def get_random_coords():
-    lat = -35 + random.random() * 10
-    lon = -71 + random.random() * 10
+    lat = -60 + random.random() * (56.5 - 17.5)  # Latitud aproximada de Chile (17.5°S a 56.5°S)
+    lon = -70 + random.random() * (71 - 74)  # Longitud aproximada de Chile (66°W a 75°W)
     return lat, lon
 
+# Agregar coordenadas a cada "walk"
 for walk in data['walks']:
     lat, lon = get_random_coords()
     walk['latitude'] = lat
     walk['longitude'] = lon
 
-
+# Guardar el archivo JSON actualizado
 with open('./db.json', 'w', encoding='utf-8') as file:
     json.dump(data, file, ensure_ascii=False, indent=2)
 
